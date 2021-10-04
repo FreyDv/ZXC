a92f680f444910ab887a9d3168657dc1502018bb
Daniil <ladm0401@gmail.com>
2021-10-05 00:48:59 +0300

    symphony without node-npm

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
+
+
+// cd my-project/
+// symfony server:start
+
+//cd my-project/
+//composer install
